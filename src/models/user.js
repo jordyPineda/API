65449688ca19367f3d92d388c265ed32a2639341
jordy pineda velandia <jepinedav@gmail.com>
2023-04-29 22:22:43 -0500

    first commit

---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema =  mongoose.Schema({
+    name: {
+        type : String,
+        require: true
+    },
+    age: {
+        type: Number,
+        require: true
+    },
+    email:{
+        type : String,
+        require: true
+    },
+    type: {
+        type : String,
+        require: true
+    }
+});
+
+module.exports = mongoose.model('user', userSchema)
---
+++ src/routes/user.js
@@ -0,0 +1,53 @@
+const express= require('express');
+const userSchema = require("../models/user")
+
+const router = express.Router();
+
+//crear usuario
+router.post('/users', (req, res)=>{
+    const user = userSchema(req.body);
+    user.save()
+        .then((data)=> res.json(data))
+        .catch((error)=>res.json({message: error}))
+})
+
+//obtener todos usuarios
+router.get('/users', (req, res)=>{
+    userSchema
+        .find()
+        .then((data)=> res.json(data))
+        .catch((error)=>res.json({message: error}))
+})
+
+
+//obtener un usuario
+router.get('/users/:id', (req, res)=>{
+    const { id } = req.params; 
+    userSchema
+        .findById(id)
+        .then((data)=> res.json(data))
+        .catch((error)=>res.json({message: error}))
+})
+
+
+//actualizar un usuario
+router.put('/users/:id', (req, res)=>{
+    const { id } = req.params; 
+    const { name,age,email,type } = req.body; 
+    userSchema
+        .updateOne({ _id:id},{$set:{name,age,email,type}})
+        .then((data)=> res.json(data))
+        .catch((error)=>res.json({message: error}))
+})
+
+//eliminar un usuario
+router.delete('/users/:id', (req, res)=>{
+    const { id } = req.params;
+    userSchema
+        .findByIdAndRemove({ _id:id})
+        .then((data)=> res.json(data))
+        .catch((error)=>res.json({message: error}))
+})
+
+
+module.exports = router;
